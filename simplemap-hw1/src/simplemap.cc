bb85b473ff78bf8cb1a3075405cb886d1220d480
jiz322 <jiz322@lehigh.edu>
2021-09-07 18:12:06 -0400

    2

--- simplemap-hw1/src/simplemap.cc
+++ simplemap-hw1/src/simplemap.cc
@@ -2,45 +2,52 @@
 
 //Since header file is the header of cc file, 
 //I try to omit many declarations here
-template <class K, class V>
-class simplemap_t{
 
-    // Define the two vectors of types K and V
-	// << use std::vector<K> >>
-    std::vector<K> k;
-    std::vector<V> v;
+//template <class K, class V>
+//^redefinition of ‘class simplemap_t<K, V>’
 
-    public:
-    // The constructor should just initialize the vectors to be empty
-    simplemap_t() {
-        k = {};
-        v = {};
-    }
+//class simplemap_t{
+//^You actually don't need to define the class
+// a second time just to implement the functions
 
-    // Insert (key, val) if and only if the key is not currently present in
-    // the map.  Returns true on success, false if the key was
-    // already present.
-    bool insert(K key, V val) {
+// Define the two vectors of types K and V
+// << use std::vector<K> >>
+std::vector<K> k;
+std::vector<V> v;
 
-    	assert("Not Implemented");
-    	return true;
+//public:
+//^this should be useless
 
-    	// The following is just an example of using C++11 features,
-    	// like the 'auto' type and lambda expression
-    	/*
-        for (auto i = keys->begin(); i != keys->end(); ++i)
-            if (*i == key)
-                return false;
-
-        // insert the key and value.  Note that indices should, by default, match
-        // between these two vectors.
-        keys->push_back(key);
-        values->push_back(val);
-		*/
-
-
-    }
+// The constructor should just initialize the vectors to be empty
+simplemap::simplemap_t() {
+    k = {};
+    v = {};
 }
+
+// Insert (key, val) if and only if the key is not currently present in
+// the map.  Returns true on success, false if the key was
+// already present.
+bool simplemap::insert(K key, V val) {
+
+    assert("Not Implemented");
+    return true;
+
+    // The following is just an example of using C++11 features,
+    // like the 'auto' type and lambda expression
+    /*
+    for (auto i = keys->begin(); i != keys->end(); ++i)
+        if (*i == key)
+            return false;
+
+    // insert the key and value.  Note that indices should, by default, match
+    // between these two vectors.
+    keys->push_back(key);
+    values->push_back(val);
+    */
+
+
+}
+
 int main(int argc, char** argv) {
 
     simplemap_t a = simplemap_t()
