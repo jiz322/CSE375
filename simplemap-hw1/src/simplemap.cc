af95a64bc200b7f87515d108d881a379404e7cc1
jiz322 <jiz322@lehigh.edu>
2021-09-07 19:50:26 -0400

    14

--- simplemap-hw1/src/simplemap.cc
+++ simplemap-hw1/src/simplemap.cc
@@ -35,7 +35,7 @@ simplemap_t:: simplemap_t() {
 // already present.
 template <class K, class V> //have to declare its template when implementing
                             //its fuctions
-simplemap_t<K, V>::bool insert(K key, V val){
+bool simplemap_t<K, V>::insert(K key, V val){
 
     assert("Not Implemented");
     return true;
