112a3049d5395c37a39ddeb9d34f784ad8ad325e
jiz322 <jiz322@lehigh.edu>
2021-09-07 19:54:49 -0400

    18

--- simplemap-hw1/src/simplemap.cc
+++ simplemap-hw1/src/simplemap.cc
@@ -33,8 +33,9 @@ simplemap_t:: simplemap_t() {
 // Insert (key, val) if and only if the key is not currently present in
 // the map.  Returns true on success, false if the key was
 // already present.
-template <class K, class V> 
-simplemap_t::insert(K key, V val){
+template <class K, class V> //have to declare its template when implementing
+                            //its fuctions
+bool simplemap_t<K, V>::insert(K key, V val){
 
     assert("Not Implemented");
     return true;
