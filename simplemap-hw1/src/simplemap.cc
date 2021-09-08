0644b41e64e52dbc65acffae025eb0b2b0ba6dad
jiz322 <jiz322@lehigh.edu>
2021-09-07 21:15:38 -0400

    fw

--- simplemap-hw1/src/simplemap.cc
+++ simplemap-hw1/src/simplemap.cc
@@ -3,8 +3,8 @@
 int main(int argc, char** argv) {
 
     simplemap_t<char*, int> a = simplemap_t<char*, int> ();
-    char* j = "jordan";
-    char* j2 = "jord";
+    char j[] = "jordan";
+    char j2[] = "jord";
     a.insert(j, 1);
     a.insert(j2, 1);
     a.insert(j, 2);
