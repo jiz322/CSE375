0c5ff27aee94b1d435dbd65d5896be11f9dc221c
jiz322 <jiz322@lehigh.edu>
2021-09-07 21:14:43 -0400

    fix waringing

--- simplemap-hw1/src/simplemap.cc
+++ simplemap-hw1/src/simplemap.cc
@@ -3,9 +3,11 @@
 int main(int argc, char** argv) {
 
     simplemap_t<char*, int> a = simplemap_t<char*, int> ();
-    a.insert("Jordan", 1);
-    a.insert("Jord", 1);
-    a.insert("Jordan", 2);
+    char* j = "jordan";
+    char* j2 = "jord";
+    a.insert(j, 1);
+    a.insert(j2, 1);
+    a.insert(j, 2);
     std::cout << &a.values << std::endl;
 
 }
