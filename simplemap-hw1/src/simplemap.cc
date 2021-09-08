57b37f01068ff388cb69ad9253ee6e35576d0e99
jiz322 <jiz322@lehigh.edu>
2021-09-08 18:19:33 -0400

    ud

--- simplemap-hw1/src/simplemap.cc
+++ simplemap-hw1/src/simplemap.cc
@@ -6,6 +6,7 @@ int main(int argc, char** argv) {
     simplemap_t<char*, int> a = simplemap_t<char*, int> ();
     char j[] = "jordan";
     char j2[] = "jord";
+    char j3[] = "jordan";
     a.insert(j, 1);
     a.insert(j2, 11);
     a.insert(j, 2);
@@ -13,7 +14,9 @@ int main(int argc, char** argv) {
     //The  "[1]" dereference the list, can also do with starting "*"
     printf ("%d",(a.values->data())[0]); // Result 11
     printf ("%d",a.values->size()); // Result 2
+    //CANNOT PASS STRING DIRECTLY!!!!!!!!!
     printf ("%d",a.update(j, 666)); 
     printf ("%d",(a.values->data())[0]); // Result 11
+    printf ("%d",a.update(j3, 666)); 
 
 }
