04ac1d437b6febf20f234153ca57a05639aa5fb7
jiz322 <jiz322@lehigh.edu>
2021-09-08 18:00:46 -0400

    db

--- simplemap-hw1/src/simplemap.h
+++ simplemap-hw1/src/simplemap.h
@@ -83,7 +83,7 @@ class simplemap_t {
     bool update(K key, V val) {
         for (auto i = keys->begin(); i != keys->end(); ++i)
             if (*i == key)
-                j = values->begin();
+                auto j = values->begin();
                 //Compute the location of destination value
                 j = j + (i - keys->begin());
                 //Replace
