64bdedfe784cf9bbd867a398211b04e1217d48a2
jiz322 <jiz322@lehigh.edu>
2021-09-07 21:03:33 -0400

    3.011

--- simplemap-hw1/src/simplemap.cc
+++ simplemap-hw1/src/simplemap.cc
@@ -3,6 +3,9 @@
 int main(int argc, char** argv) {
 
     simplemap_t<char*, int> a = simplemap_t<char*, int> ();
-    std::cout << "hi" << std::endl;
+    a.insert("Jordan", 1);
+    a.insert("Jord", 1);
+    a.insert("Jordan", 2);
+    std::cout << &a.values << std::endl;
 
 }
