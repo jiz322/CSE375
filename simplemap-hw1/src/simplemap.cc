5e7fff35620ce30adb7ce8ddf1272ae61bd93726
jiz322 <jiz322@lehigh.edu>
2021-09-08 18:26:15 -0400

    df

--- simplemap-hw1/src/simplemap.cc
+++ simplemap-hw1/src/simplemap.cc
@@ -7,16 +7,28 @@ int main(int argc, char** argv) {
     char j[] = "jordan";
     char j2[] = "jord";
     char j3[] = "jordan";
+    char j4[] = "jordan";
+    char j5[] = "jordan";
+    char j6[] = "jordan";
+    char j7[] = "jordan";
     a.insert(j, 1);
     a.insert(j2, 11);
     a.insert(j, 2);
+    a.insert(j4, 4);
+    a.insert(j5, 5);
+    a.insert(j6, 6);
+    a.insert(j7, 7);
+    a.insert(j3, 8);
     //dereference using '->', deref the values pointer to exact vector
     //The  "[1]" dereference the list, can also do with starting "*"
-    printf ("%d",(a.values->data())[0]); // Result 11
+    printf ("%d",(a.values->data())[0]); 
     printf ("%d",a.values->size()); // Result 2
     //CANNOT PASS STRING DIRECTLY!!!!!!!!!
     printf ("%d",a.update(j, 666)); 
-    printf ("%d",(a.values->data())[0]); // Result 11
-    printf ("%d",a.update(j3, 666)); 
+    printf ("%d",(a.values->data())[6]); 
+    printf ("%d",a.update(j3, 888)); 
+    printf ("%d",(a.values->data())[6]); 
+
+
 
 }
