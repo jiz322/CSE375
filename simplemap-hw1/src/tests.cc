534746dfb6335565b45352a932433a6e2cd2d6c8
jiz322 <jiz322@lehigh.edu>
2021-09-09 21:59:35 -0400

    2

--- simplemap-hw1/src/tests.cc
+++ simplemap-hw1/src/tests.cc
@@ -163,7 +163,7 @@
 		// Which optimization can you do to the single-threaded execution in
 		// order to improve its performance?
 		timer stopwatch2;
-		for (int i = 0; i < cfg.iters; i++){
+		for (int i = 0; i < cfg.iters*cfg.threads; i++){
 			if (dist100(rng) < 95){
 				deposit();
 			}
