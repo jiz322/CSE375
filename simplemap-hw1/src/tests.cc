794a34d9d98f30880404c2400da3edcb3e9ac881
jiz322 <jiz322@lehigh.edu>
2021-09-10 21:06:33 -0400

    10

--- simplemap-hw1/src/tests.cc
+++ simplemap-hw1/src/tests.cc
@@ -85,9 +85,9 @@
 		// Jordan: "Let's have 10 accounts each 10000.00 bucks"
 		int max_accounts = cfg.key_max;
 		for (int i = 0; i < max_accounts - 1; i++){
-			map.insert(i, 100000.0/max_accounts);
+			map.insert(i, 90000.0/max_accounts);
 		}
-        map.insert(max_accounts-1, 1000000.0 - balance());
+        map.insert(max_accounts-1, 100000.0 - balance());
 		// Step 3
 		// Define a function "deposit" that selects two random bank accounts
 		// and an amount. This amount is subtracted from the amount
