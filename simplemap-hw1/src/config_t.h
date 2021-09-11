62f2067f6b6333d1c6039270d1dadf82de587ee1
jiz322 <jiz322@lehigh.edu>
2021-09-10 22:02:42 -0400

    6

--- simplemap-hw1/src/config_t.h
+++ simplemap-hw1/src/config_t.h
@@ -23,7 +23,7 @@ struct config_t {
     int threads;
 
     // simple constructor
-    config_t() : key_max(2560), iters(512), name("10 partisan"), threads(16) { }
+    config_t() : key_max(2560), iters(1365), name("10 partisan"), threads(6) { }
 
     // Print the values of the iters, and name fields
     void dump();
